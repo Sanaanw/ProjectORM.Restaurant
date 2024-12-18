0cb908a05d976c8fdc59b386a50af65c0289c7cd
Nesrullayev <senan.nesrullayevv@gmail.com>
2024-12-18 23:18:19 +0400

    OptimizingMethod

--- Restaurant/Restaurant.App/Program.cs
+++ Restaurant/Restaurant.App/Program.cs
@@ -8,62 +8,70 @@ namespace Restaurant.App
     {
         static void Main(string[] args)
         {
-            try
+            bool MainLoop = true;
+            while (MainLoop)
             {
-                MenuItemController menuItemController = new();
-                OrderController orderController = new();
-                ManageMenuItems manageMenuItems = new();
-                ManageOrders manageOrders = new();
-                bool result = true;
-                do
+                try
                 {
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("Menu:");
-                    Console.ResetColor();
-                    Console.WriteLine("1.Perform operation on the menu.");
-                    Console.WriteLine("2.Perform operation on orders.");
-                    Console.ForegroundColor= ConsoleColor.Red;
-                    Console.WriteLine("0.Exit ");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine( );
-                    Console.Write("Select your option: ");
-                    Console.ResetColor();
-                    var input = Console.ReadLine();
-                    if (!int.TryParse(input, out int answer))
-                        answer = 100;
-                    bool MenuResult = true;
-                    switch (answer)
+                    MenuItemController menuItemController = new();
+                    OrderController orderController = new();
+                    ManageMenuItems manageMenuItems = new();
+                    ManageOrders manageOrders = new();
+                    bool result = true;
+                    do
                     {
-                        case 1:
-                            manageMenuItems.ManageMenuItem();
-                            break;
-                        case 2:
-                            manageOrders.ManageOrder();
-                            break;
-                        case 0:
-                            Console.WriteLine("Existing menu...");
-                            Thread.Sleep(1000);
-                            Console.Clear( );   
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Program ended");
-                            Console.ResetColor();
-                            result = false;
-                            break;
-                        default:
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.WriteLine("Pls Add appropriate command");
-                            Console.ResetColor();
-                            Thread.Sleep(2000);
-                            break;
-                    }
-                } while (result);
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine(ex.Message);
-                Console.ResetColor();
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Menu:");
+                        Console.ResetColor();
+                        Console.WriteLine("1.Perform operation on the menu.");
+                        Console.WriteLine("2.Perform operation on orders.");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("0.Exit ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine();
+                        Console.Write("Select your option: ");
+                        Console.ResetColor();
+                        var input = Console.ReadLine();
+                        if (!int.TryParse(input, out int answer))
+                            answer = 100;
+                        bool MenuResult = true;
+                        switch (answer)
+                        {
+                            case 1:
+                                manageMenuItems.ManageMenuItem();
+                                break;
+                            case 2:
+                                manageOrders.ManageOrder();
+                                break;
+                            case 0:
+                                Console.WriteLine("Existing menu...");
+                                Thread.Sleep(1000);
+                                Console.Clear();
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Program ended");
+                                Console.ResetColor();
+                                result = false;
+                                break;
+                            default:
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.WriteLine("Pls Add appropriate command");
+                                Console.ResetColor();
+                                Thread.Sleep(2000);
+                                break;
+                        }
+                    } while (result);
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"Error: {ex.Message}");
+                    Console.WriteLine();
+                    Console.ForegroundColor= ConsoleColor.Green;
+                    Console.WriteLine(" - Click here and try again");
+                    Console.ReadLine();
+                    Console.ResetColor();
+                }
             }
         }
     }
--- Restaurant/Restaurant.Service/obj/Debug/net8.0/Restaurant.Service.AssemblyInfo.cs
+++ Restaurant/Restaurant.Service/obj/Debug/net8.0/Restaurant.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d47675ccecaa51cf4f599e624a594b7f4e67fcf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57f0ba7ffcdfca6f464d1f5a0fbe6d751cad5115")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
